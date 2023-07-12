1c6dd539c013cd9e6407716e25591b0acd0e5f71
adriquint <125844291+adriquint@users.noreply.github.com>
2023-07-11 22:26:13 -0300

    Add files via upload

---
+++ ejercicios_profundizacion/js/app.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const resultText = document.querySelector("#resultText")
+const imgPoke = document.querySelector("#imgpokemon")
+const input = document.querySelector("input")
+const puntaje = document.getElementById("puntaje")
+
+let numero = 0;
+
+// -- Leer valor de Session Storage --
+let acertadosStorage = (parseInt(sessionStorage.getItem("acertados")))
+let encuestadosStorage = parseInt(sessionStorage.getItem("encuestados"))
+console.log(acertadosStorage)
+console.log(encuestadosStorage)
+
+let cantidadAcertados =  acertadosStorage > 0 ? acertadosStorage : 0;  
+let cantidadEncuestados = encuestadosStorage > 0 ? encuestadosStorage : 0; 
+console.log(cantidadAcertados)
+console.log(cantidadEncuestados)
+
+// -- Leer datos Json --
+const data = JSON.parse(jsonData)
+//console.log(data)
+
+// -- Armar lista pokemons y crear objetos Pokemon -- (clase6, carp7.2)
+let pokemons = [];
+for(const pokeData of data) {
+    pokemons.push(new Pokemon(
+                    pokeData.id,
+                    pokeData.name,
+                    pokeData.thumbnail
+                        )
+                );
+}
+//console.log(pokemons)
+
+// -- Programa trabaja con lista de objetos pokemons --
+
+function restart() {    
+    resultText.textContent = ""
+    imgPoke.classList.remove("success")
+    numero = Math.round(Math.random()*(pokemons.length));
+    imgPoke.src = (pokemons[numero].thumbnail);
+    input.value = "";
+
+    //Pruebas
+    console.log(`Numero random: ${numero}`);
+    console.log(`"Id" poke: ${pokemons[numero].id}`)
+    console.log(`Nombre poke: ${pokemons[numero].name}`); 
+}
+
+function resultado() {
+    console.log("--Funcion resultado--");
+    cantidadEncuestados = (cantidadEncuestados + 1);
+        
+    (input.value) === (pokemons[numero].name) ? (
+        resultText.textContent = "-> ¡Felicitaciones! Respuesta correcta",
+        imgPoke.classList.add("success"),
+        cantidadAcertados = cantidadAcertados + 1,
+        console.log("Respuesta correcta")
+    ) : (
+        resultText.textContent = "-> RESPUESTA INCORRECTA",
+        imgPoke.classList.remove("success"),
+        console.log("Respuesta incorrecta")
+        );
+    
+    (cantidadAcertados) >= (cantidadEncuestados / 2) ? (
+        puntaje.classList.remove("puntajeBajo"),
+        console.log("Puntaje bueno")
+    ) : (
+        puntaje.classList.add("puntajeBajo"),
+        console.log("Puntaje bajo")
+    );
+    puntaje.innerText = `Puntaje ${cantidadAcertados} de ${cantidadEncuestados}`
+    
+    //Pruebas
+    console.log(`Cantidad de encuestados: ${cantidadEncuestados}`);
+    console.log(`Cantidad de acertados: ${cantidadAcertados}`);
+}
+console.log(`Cantidad de encuestados: ${cantidadEncuestados}`);
+console.log(`Cantidad de acertados: ${cantidadAcertados}`);
+restart();
---
+++ ejercicios_profundizacion/js/events.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const send = document.getElementById('send')
+const reload = document.getElementById('reload')
+
+reload.onclick = () => restart()
+
+send.onclick = () => {
+  input.onchange = () => resultado()
+  sessionStorage.setItem("acertados", cantidadAcertados),
+  sessionStorage.setItem("encuestados", cantidadEncuestados) 
+}
